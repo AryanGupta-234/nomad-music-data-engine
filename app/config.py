1187e3166448774f2f1faf1f6c8a76573a04edef
Aryan Gupta <aassworld99@gmail.com>
2026-08-18 09:35:36 +0530

    Add configurable YouTube sync size

--- app/config.py
+++ app/config.py
@@ -6,6 +6,7 @@ class Settings(BaseSettings):
     youtube_client_id: str = ""
     youtube_client_secret: str = ""
     youtube_redirect_uri: str = "http://127.0.0.1:8787/auth/youtube/callback"
+    youtube_sync_max_items: int = 5000
     user_api_base_url: str = "http://127.0.0.1:8765"
     user_api_token: str = ""
     database_path: str = "data/nomad_music.db"
